fdd22905a184ecd9eba549a6aa607a2f8c954aca
kubassek <Internet072405>
2017-01-24 20:04:41 +0000

    About to mess the angles up

--- src/EvolutionaryAlgorithm.java
+++ src/EvolutionaryAlgorithm.java
@@ -1,63 +1,97 @@
 import java.awt.Point;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Random;
-import java.io.*;
+import java.util.*;
+
 
 import javax.swing.JFrame;
 
 
-public class EvolutionaryAlgorithm{
+public class EvolutionaryAlgorithm {
 
-    private Random rand = new Random();
+    public static PhysicsSystem phy = new PhysicsSystem();
+    public static Wind wind = new Wind();
     private static Point start = new Point(150, 20);
     private static Point finish = new Point(150, 400);
     private static int populationSize = 100;
-    private ArrayList<Solution> population = new ArrayList<Solution>();
-    public static PhysicsSystem phy = new PhysicsSystem();
-    public static Wind wind = new Wind();
     public JFrame f;
     public Gui panel = new Gui();
+    private Random rand = new Random();
+    private ArrayList<Solution> population = new ArrayList<Solution>();
 
     public void runAlgorithm() throws Exception {
 
-        wind.to = new Point(50,100);
-        wind.from = new Point(50,10);
+        wind.to = new Point(50, 100);
+        wind.from = new Point(50, 10);
         //	wind.setWindSpeed(4);
 
         wind.generateWindSpeed();
         //	wind.generatePoints();
         wind.setWindAngle(phy.getAngleFromPoint(wind.from, wind.to));
 
-        System.out.println("The wind angle is: " + wind.getWindAngle());
-        System.out.println("The wind speed is: " + wind.getWindSpeed());
+        //System.out.println("The wind angle is: " + wind.getWindAngle());
+        // System.out.println("The wind speed is: " + wind.getWindSpeed());
 
         initialisePopulation();
-        System.out.println(population.size());
-        System.out.println(this.getBestSol().getSolution().size() + " Time: " + this.calculateTime((this.getBestSol())));
+        // System.out.println(population.size());
+        // System.out.println(this.getBestSol().getSolution().size() + " Time: " + this.calculateTime((this.getBestSol())));
 
-        f = new JFrame ();
+        f = new JFrame();
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.getContentPane().add (panel);
+        f.getContentPane().add(panel);
         f.setSize(1080, 720);
         // Show the frame.
-        f.setVisible (true);
+        f.setVisible(true);
 
-        for(Solution s:population){
-            //	this.showGui(s, wind);
-            //	Thread.sleep(100);
+        for (Solution s : population) {
+            this.showGui(s, wind);
+/*
+                System.out.println(s.getSolutionTime() + "\n" + "==================================================");
+                for(int i=0; i<s.getSolution().size()-1;i++) {
+
+                        double angleBetweenPoints = phy.getAngleFromPoint(s.getSolution().get(i), s.getSolution().get(i + 1));
+                        double boatWindAngle = angleBetweenPoints;
+
+                        if(boatWindAngle > 180){
+                            boatWindAngle = 360 - boatWindAngle;
+                        }
+                        System.out.print(boatWindAngle + " || ");
+
+
+                }
+*/
+
+            System.out.println((Math.atan2((wind.to.x - wind.from.x), (wind.to.y - wind.from.y)) * 180 / Math.PI));
+
+            for (int i = 0; i < s.getSolution().size() - 1; i++) {
+
+                double angleBetweenPoints = phy.getAngleFromPoint(s.getSolution().get(i), s.getSolution().get(i + 1));
+               // System.out.println("angle between points = " + (angleBetweenPoints));
+               // System.out.println("angleBetweenPoints - windAngle = " + (angleBetweenPoints-wind.getWindAngle()) + "\n\n");
+
+                double angle2 = (Math.atan2((s.getSolution().get(i + 1).x - s.getSolution().get(i).x), (s.getSolution().get(i + 1).y - s.getSolution().get(i).y)) * 180 / Math.PI);
+                System.out.println("secondPoint.x > firstPoint.x = " + angle2);
+                //System.out.println("angle - windAngle = " + (angle2 + wind.getWindAngle()) + "\n\n");
+                /*
+                double angle1 = (Math.atan2((s.getSolution().get(i).x - s.getSolution().get(i + 1).x), (s.getSolution().get(i).y - s.getSolution().get(i + 1).y)) * 180 / Math.PI);
+                System.out.println("secondPoint.x < firstPoint.x = " + angle1 + "\n\n");
+                System.out.println("angle - windAngle = " + (angle1 + wind.getWindAngle()));
+                */
+
+            }
+            Thread.sleep(100000000);
         }
 
 
-        for(int x=0; x<20000000;x++){
+        for (int x = 0; x < 20000; x++) {
             Solution parent1 = findParent();
             Solution parent2 = findParent();
             Solution child = crossOver(parent1, parent2);
             this.showGui(child, wind);
+
             //this.checkSolution(child);
-            if(this.calculateTime(child)<this.calculateTime(this.getWorstSol())){
+            if (this.calculateTime(child) < this.calculateTime(this.getWorstSol())) {
                 System.out.println(this.calculateTime(child));
                 population.add(child);
 
@@ -71,12 +105,12 @@ public class EvolutionaryAlgorithm{
         this.showGui(this.getBestSol(), wind);
     }
 
-    private Solution findParent(){
+    private Solution findParent() {
         Solution parent = null;
 
         this.sortPopulation();
 
-        for(Solution sol: population){
+        for (Solution sol : population) {
             //System.out.println(sol.getSolutionTime() + " " + sol.getSolution().size());
         }
 
@@ -86,22 +120,24 @@ public class EvolutionaryAlgorithm{
         return parent;
     }
 
-    private Solution crossOver(Solution parent1, Solution parent2){
+    private Solution crossOver(Solution parent1, Solution parent2) {
         Solution child = new Solution();
         int points = 0;
 
-        if(parent1.getSolution().size() > parent2.getSolution().size()){
+        if (parent1.getSolution().size() > parent2.getSolution().size()) {
             points = parent1.getSolution().size();
-        }else points = parent2.getSolution().size();
+        } else points = parent2.getSolution().size();
         child.addPoint(start);
 
-        for(int i=1; i<(points-1); i++){
+        for (int i = 1; i < (points - 1); i++) {
             int chance = rand.nextInt(2);
-            if(chance == 1 && parent1.getSolution().size()>i){
+            if (chance == 1 && parent1.getSolution().size() > i) {
                 child.addPoint(parent1.getSolution().get(i));
-            } else if (parent2.getSolution().size()>i){
+            } else if (parent2.getSolution().size() > i) {
                 child.addPoint(parent2.getSolution().get(i));
-            } else{child.addPoint(parent1.getSolution().get(i));}
+            } else {
+                child.addPoint(parent1.getSolution().get(i));
+            }
         }
 
         child.setSolutionTime(this.calculateTime(child));
@@ -111,35 +147,35 @@ public class EvolutionaryAlgorithm{
     }
 
 
-    private Solution mutate(Solution sol){
+    private Solution mutate(Solution sol) {
         Solution solution = sol;
 
         int randMutation = rand.nextInt(20);
         int randDirection = rand.nextInt(4);
         int randDistance = rand.nextInt(20);
 
-        if(randMutation == 1){
+        if (randMutation == 1) {
             int randPoint = rand.nextInt(solution.getSolution().size());
-            if(randDirection == 0){
-                if(solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish){
+            if (randDirection == 0) {
+                if (solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish) {
                     double locationX = solution.getSolution().get(randPoint).getX();
                     double locationY = solution.getSolution().get(randPoint).getY();
-                    solution.getSolution().get(randPoint).setLocation(locationX-randDistance, locationY);
+                    solution.getSolution().get(randPoint).setLocation(locationX - randDistance, locationY);
                 }
-            }else if(randDirection == 1){
-                if(solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish){
+            } else if (randDirection == 1) {
+                if (solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish) {
                     double locationX = solution.getSolution().get(randPoint).getX();
                     double locationY = solution.getSolution().get(randPoint).getY();
-                    solution.getSolution().get(randPoint).setLocation(locationX+randDistance, locationY);
+                    solution.getSolution().get(randPoint).setLocation(locationX + randDistance, locationY);
                 }
-            }else if(randDirection == 2){
-                if(solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish){
+            } else if (randDirection == 2) {
+                if (solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish) {
                     double locationX = solution.getSolution().get(randPoint).getX();
                     double locationY = solution.getSolution().get(randPoint).getY();
                     solution.getSolution().get(randPoint).setLocation(locationX, locationY - randDistance);
                 }
-            }else if(randDirection == 3){
-                if(solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish){
+            } else if (randDirection == 3) {
+                if (solution.getSolution().get(randPoint) != start && solution.getSolution().get(randPoint) != finish) {
                     double locationX = solution.getSolution().get(randPoint).getX();
                     double locationY = solution.getSolution().get(randPoint).getY();
                     solution.getSolution().get(randPoint).setLocation(locationX, locationY + randDistance);
@@ -151,43 +187,46 @@ public class EvolutionaryAlgorithm{
     }
 
 
-    public boolean checkSolution(Solution solution){
+    public boolean checkSolution(Solution solution) {
 
         boolean correct = true;
 
-        for(int i=0; i<solution.getSolution().size()-1;i++){
+        for (int i = 0; i < solution.getSolution().size() - 1; i++) {
             double angleBetweenPoints = 0;
             double windAngle = wind.getWindAngle();
             double boatWindAngle = 0;
 
-            angleBetweenPoints = phy.getAngleFromPoint(solution.getSolution().get(i), solution.getSolution().get(i+1));
+
+            angleBetweenPoints = phy.getAngleFromPoint(solution.getSolution().get(i), solution.getSolution().get(i + 1));
             boatWindAngle = angleBetweenPoints - windAngle;
 
-            if(boatWindAngle < 0){
+
+            if (boatWindAngle < 0) {
                 boatWindAngle = Math.abs(boatWindAngle);
             }
 
-            if(boatWindAngle > 180){
+            if (boatWindAngle > 180) {
                 boatWindAngle = 360 - boatWindAngle;
             }
 
-            if(boatWindAngle<32){
+            if (boatWindAngle < 32) {
                 correct = false;
             }
-            System.out.println(boatWindAngle);
+            //  System.out.println(boatWindAngle);
         }
-        if(correct == false){
-            System.out.println("Deleted");
+        if (correct == false) {
+            //  System.out.println("Deleted");
         }
-        System.out.println("==========================================");
+        //  System.out.println("==========================================");
         return correct;
 
     }
 
 
-    private void sortPopulation(){
+    private void sortPopulation() {
         Collections.sort(population, new Comparator<Solution>() {
-            @Override public int compare(final Solution sol1, final Solution sol2) {
+            @Override
+            public int compare(final Solution sol1, final Solution sol2) {
                 if (sol1.getSolutionTime() > sol2.getSolutionTime()) {
                     return 1;
                 } else if (sol1.getSolutionTime() < sol2.getSolutionTime()) {
@@ -198,22 +237,22 @@ public class EvolutionaryAlgorithm{
         });
     }
 
-    private Solution getWorstSol(){
+    private Solution getWorstSol() {
         Solution worst = null;
 
-        for(Solution sol : population){
-            if(worst == null || this.calculateTime(worst) < this.calculateTime(sol)){
+        for (Solution sol : population) {
+            if (worst == null || this.calculateTime(worst) < this.calculateTime(sol)) {
                 worst = sol;
             }
         }
         return worst;
     }
 
-    private Solution getBestSol(){
+    private Solution getBestSol() {
         Solution best = null;
 
-        for(Solution sol : population){
-            if(best == null || this.calculateTime(best) > this.calculateTime(sol)){
+        for (Solution sol : population) {
+            if (best == null || this.calculateTime(best) > this.calculateTime(sol)) {
                 best = sol;
             }
         }
@@ -221,14 +260,19 @@ public class EvolutionaryAlgorithm{
         return best;
     }
 
-    private void initialisePopulation(){
+    private void initialisePopulation() {
         //Create 100 solutions and add them to the solution
-        for(int p=0; p<populationSize; p++){
+        for (int p = 0; p < populationSize; p++) {
             Solution path = new Solution();
 
-            Point midpoing;
+            Point midpoint = new Point();
+            int radius = (int) (Math.round(start.distance(finish) - (start.distance(finish) / 2)) + (start.distance(finish) / 3));
 
-            
+            int x = (start.x + finish.x) / 2;
+            int y = (start.y + finish.y) / 2;
+
+            midpoint.x = x;
+            midpoint.y = y;
 
             int max = 14;
             int min = 1;
@@ -236,18 +280,26 @@ public class EvolutionaryAlgorithm{
 
             path.addPoint(start);
 
-            for(int i=0; i<numberOfTurns; i++ ){
+            for (int i = 0; i < numberOfTurns; i++) {
                 Point randomPoint = new Point();
-                randomPoint.x = rand.nextInt(((start.x + 400) - (start.x - 400)) + 1) + (start.x - 400);
-                randomPoint.y = rand.nextInt(((finish.y) - (start.y)) + 1) + (start.y);
+
+
+                double angle = Math.random() * Math.PI * 2;
+                double radiusRandom = rand.nextDouble() * radius;
+                double xRandom = midpoint.x + radiusRandom * Math.cos(angle);
+                double yRandom = midpoint.y + radiusRandom * Math.sin(angle);
+
+                randomPoint.x = (int) xRandom;
+                randomPoint.y = (int) yRandom;
+
                 path.addPoint(randomPoint);
             }
 
             path.addPoint(finish);
 
-            if(this.checkSolution(path)==true){
+            if (this.checkSolution(path) == true) {
                 population.add(path);
-            }else{
+            } else {
                 p--;
             }
             path.setSolutionTime(this.calculateTime(path));
@@ -256,19 +308,18 @@ public class EvolutionaryAlgorithm{
     }
 
 
-
-    private double calculateTime(Solution sol){
+    private double calculateTime(Solution sol) {
         double soulutionTime = 0;
 
-        for(int x=0;x<sol.getSolution().size()-1;x++){
+        for (int x = 0; x < sol.getSolution().size() - 1; x++) {
 
-            soulutionTime += phy.timeBetweenPoints(sol.getSolution().get(x), sol.getSolution().get(x+1), wind);
+            soulutionTime += phy.timeBetweenPoints(sol.getSolution().get(x), sol.getSolution().get(x + 1), wind);
 
         }
         return soulutionTime;
     }
 
-    public void showGui(Solution solution, Wind wind){
+    public void showGui(Solution solution, Wind wind) {
 
         panel.repaintSolution(solution, wind);
         panel.repaint();
